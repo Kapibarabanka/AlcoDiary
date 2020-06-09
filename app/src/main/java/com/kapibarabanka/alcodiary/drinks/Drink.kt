4aea6b06f41725aba0b66bd467f008d3defe0792
lunosgoode <lunosgoode@gmail.com>
2020-06-10 02:31:22 +0300

    Added star-bar to pop-ups, changed widgets positions and colour scheme

--- app/src/main/java/com/kapibarabanka/alcodiary/drinks/Drink.kt
+++ app/src/main/java/com/kapibarabanka/alcodiary/drinks/Drink.kt
@@ -4,4 +4,5 @@ class Drink(var name: String, var type: DrinkType, mark: Int, comment: String = 
     var alco: Int = type.defaultAlco
     var mark: Int = mark
     var comment: String = comment
+    var rating : Int = 0    // This value will be updated, when user rates drink
 }
